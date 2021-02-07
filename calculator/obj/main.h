c16a5b510a6e6e6f7ccf2889c95774fbbe627a2f
Pepe González <pepeglezabad@gmail.com>
2021-02-07 21:32:30 +0100

    Recursive func?

--- calculator/obj/main.h
+++
@@ -1,3 +0,0 @@
-obj/main.o: src/main.cpp src/../include/utils.h
-src/main.cpp:
-src/../include/utils.h:
--- calculator/obj/utils.h
+++
@@ -1,3 +0,0 @@
-obj/utils.o: src/utils.cpp src/../include/utils.h
-src/utils.cpp:
-src/../include/utils.h:
