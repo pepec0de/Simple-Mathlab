863c7ab617657d918583fc7de0b68666d9e2ca07
Pepe González <pepeglezabad@gmail.com>
2021-03-11 13:59:08 +0100

    Added a operator check in the splitBySigns func

--- calculator/obj/main.h
+++
@@ -1,6 +0,0 @@
-obj/main.o: src/main.cpp src/../include/stringutils.h src/../include/utils.h \
- src/../include/stringutils.h
-src/main.cpp:
-src/../include/stringutils.h:
-src/../include/utils.h:
-src/../include/stringutils.h:
--- calculator/obj/utils.h
+++
@@ -1,5 +0,0 @@
-obj/utils.o: src/utils.cpp src/../include/utils.h \
- src/../include/stringutils.h
-src/utils.cpp:
-src/../include/utils.h:
-src/../include/stringutils.h:
